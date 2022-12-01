cf05b06d9caa90895558e90e2d6c8fb3d3fced4c
blckvia <a.vyatkinn@icloud.com>
2022-12-01 22:41:05 +0500

    Redux implemented

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -2,7 +2,9 @@ const { combineReducers } = require('redux');
 
 /* eslint-disable */
 import { positionReducer } from './positions/position-reducer';
+import { filterReducer } from './filters/filter-reducer';
 
 export const rootReducer = combineReducers({
     positions: positionReducer,
+    filters: filterReducer,
 });
